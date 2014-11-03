532eeb797daef91acde4776c3be8f5b19a40f166
Paul Middelkoop <paul@pamisoft.nl>
2014-11-03 11:52:58 +0100

    Fixed generics in SpringVoidFuncion

--- src/main/java/eu/icolumbo/cinch/SpringVoidFunction.java
+++ src/main/java/eu/icolumbo/cinch/SpringVoidFunction.java
@@ -8,16 +8,16 @@ import org.apache.spark.api.java.function.VoidFunction;
 public class SpringVoidFunction<T> implements VoidFunction<T> {
 
     private final Class<?> springConfigurationClass;
-    private final Class<? extends VoidFunction> springBeanClass;
+    private final Class<? extends VoidFunction<T>> springBeanClass;
 
-    SpringVoidFunction(Class<?> springConfigurationClass, Class<? extends VoidFunction> springBeanClass) {
+    SpringVoidFunction(Class<?> springConfigurationClass, Class<? extends VoidFunction<T>> springBeanClass) {
         this.springConfigurationClass = springConfigurationClass;
         this.springBeanClass = springBeanClass;
     }
     
     @Override
     public void call(T t) throws Exception {
-        VoidFunction function = SpringContext.getContext(springConfigurationClass).getBean(springBeanClass);
+        VoidFunction<T> function = SpringContext.getContext(springConfigurationClass).getBean(springBeanClass);
         function.call(t);
     }
 }
